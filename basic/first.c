5dd421283507dd6d3fe41442075e629da91fef93
Philipp Matti <matti@puzzle.ch>
2016-02-02 00:25:30 +0100

    re-structure and add some makefiles

--- basic/first.c
+++ basic/first.c
@@ -2,6 +2,9 @@
 #include <stdio.h> // default input output
 
 int main(int argc, char **argv) {
-  printf("just a hallo world\n");
+
+  int years = 300;
+
+  printf("The first %d years\n", years);
   return 0;
 }
